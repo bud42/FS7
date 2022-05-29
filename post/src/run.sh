a280cb2c22d40785f438f683d847fbc9968f7e89
bud42 <bdboyd42@gmail.com>
2022-05-29 14:26:45 -0400

    Update run.sh

--- post/src/run.sh
+++ post/src/run.sh
@@ -1,10 +1,13 @@
+# Run post processing for FreeSurfer 7 recon-all to make stats and PDF for DAX
 
-# Make stats text files
+# Make stats text file
 cd /OUTPUTS/SUBJ/stats
 /opt/src/make_stats.sh
+mv /OUTPUTS/SUBJ/stats/stats.txt /OUTPUTS/
 
 # Create QA PDF
 cd /OUTPUTS/SUBJ
 xvfb-run -a --server-args "-screen 0 1920x1080x24" /opt/src/make_pdf.sh
+mv /OUTPUTS/SUBJ/report.pdf /OUTPUTS/
 
 echo "run DONE!"
